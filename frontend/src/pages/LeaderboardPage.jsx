220858b7dc1c0795c361fcdce6a2aa9f1178458b
VIJAYAPANDIANT <vijayapandian112007@gmail.com>
2026-06-26 21:26:04 +0530

    Add 10 more users to mock leaderboard

--- frontend/src/pages/LeaderboardPage.jsx
+++ frontend/src/pages/LeaderboardPage.jsx
@@ -14,7 +14,17 @@ const LeaderboardPage = () => {
       setStandings([
         { _id: '1', username: 'admin', impactScore: 500, rank: 1 },
         { _id: '2', username: 'jane_green', impactScore: 250, rank: 2 },
-        { _id: '3', username: 'john_doe', impactScore: 180, rank: 3 }
+        { _id: '3', username: 'john_doe', impactScore: 180, rank: 3 },
+        { _id: '4', username: 'eco_warrior', impactScore: 165, rank: 4 },
+        { _id: '5', username: 'sarah_smith', impactScore: 150, rank: 5 },
+        { _id: '6', username: 'green_earth', impactScore: 140, rank: 6 },
+        { _id: '7', username: 'recycle_master', impactScore: 125, rank: 7 },
+        { _id: '8', username: 'nature_lover99', impactScore: 110, rank: 8 },
+        { _id: '9', username: 'mike_clean', impactScore: 95, rank: 9 },
+        { _id: '10', username: 'planet_saver', impactScore: 80, rank: 10 },
+        { _id: '11', username: 'alex_w', impactScore: 65, rank: 11 },
+        { _id: '12', username: 'city_cleaner', impactScore: 50, rank: 12 },
+        { _id: '13', username: 'zero_waste_hero', impactScore: 35, rank: 13 }
       ]);
       setLoading(false);
     }, 400);
